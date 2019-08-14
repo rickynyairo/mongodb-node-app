f58c2a9eda91f728fa73f2f363098a0aa2049e81
rickynyairo <rickynyairo@gmail.com>
2019-08-14 23:10:14 +0300

    chore(error): configured error handling for db connection

--- src/services/database.ts
+++ src/services/database.ts
@@ -2,10 +2,10 @@ import { connect } from "mongoose";
 import config from "../config";
 
 export const connectToDatabase = () => {
-    try {
-        return connect(`${config.MONGO_DB_URL}`, { useNewUrlParser: true });
-    } catch (error) {
-        console.log("\nerror>>>\n", error);
-        process.exit(1);
-    }
-}
+  try {
+    return connect(`${config.MONGO_DB_URL}`, { useNewUrlParser: true });
+  } catch (error) {
+    console.log("\nerror>>>\n", error);
+    process.exit(1);
+  }
+};
